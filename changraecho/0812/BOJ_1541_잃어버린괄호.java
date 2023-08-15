8dbbfd5ce647e34dd53828d77cc5b1a5cf4dcb51
crcho5133 <crcho5133@gmail.com>
2023-08-15 22:22:51 +0900

    수정

--- changraecho/0812/BOJ_1541_잃어버린괄호.java
+++ changraecho/0812/BOJ_1541_잃어버린괄호.java
@@ -2,10 +2,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-public class BJ_잃어버린괄호_1541 {
+public class BOJ_1541_잃어버린괄호 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -44,10 +43,10 @@ public class BJ_잃어버린괄호_1541 {
 
 		int memSize = memory.size();
 		for (int i = 0; i < memSize - 1; i++) {
-			parentheses[memory.get(i)] = memory.get(i+1) - memory.get(i);
+			parentheses[memory.get(i)] = memory.get(i + 1) - memory.get(i);
 		}
 		if (memSize > 0) {
-			parentheses[memory.get(memSize - 1)] = pLen - memory.get(memSize -1) ;
+			parentheses[memory.get(memSize - 1)] = pLen - memory.get(memSize - 1);
 		}
 
 		// nums와 parentheses를 이용하여 ans 계산
@@ -67,10 +66,6 @@ public class BJ_잃어버린괄호_1541 {
 		}
 
 		System.out.println(ans);
-
-
-
-
 
 	}
 
